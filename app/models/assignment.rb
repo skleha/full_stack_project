d71038395b0be895ce105eca7e255fb5dacf6bf5
thejenkdog <thejenkdog@gmail.com>
2019-11-08 11:19:34 -0800

    Added assignment model validations, added data, tested successfully

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,2 +1,6 @@
 class Assignment < ApplicationRecord
+  validates :note_id, :label_id, presence: true
+  validates :note_id, uniqueness: { scope: :label_id, message: "Note already has that label"}
+
+
 end
