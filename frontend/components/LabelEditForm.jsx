4a5b8b5282a7c7172f89aca978a9f73c030d89a8
thejenkdog <thejenkdog@gmail.com>
2019-11-11 15:04:04 -0800

    add input to label edit form

--- frontend/components/LabelEditForm.jsx
+++ frontend/components/LabelEditForm.jsx
@@ -3,10 +3,43 @@ import React from 'react';
 
 class LabelEditForm extends React.Component {
 
+  constructor(props)  {
+    super(props);
+    this.state = { editMode: false };
+    this.turnOnEditMode = this.turnOnEditMode.bind(this);
+  }
+
+  turnOnEditMode(e) {
+    e.stopPropagation();
+    this.setState({ editMode: true });
+  }
 
   render() {
+
+    const checkIcon = this.state.editMode ? 
+      ( <i className="fal fa-check"></i> ) :
+      ( null )
+
     return (
-      <div>You got the form</div>
+      <div className="label-edit-form">
+        
+        <div className="label-edit-form-title">
+          Label note
+        </div>
+
+        <div className="label-edit-form-input-container">
+          <input 
+            type="text"
+            className="label-edit-form-input"
+            placeholder="Enter Label Name"
+            onClick={this.turnOnEditMode}
+          />
+          { checkIcon }
+
+        </div>
+      
+      
+      </div>
     )
   }
   
--- frontend/components/NoteIndexItem.jsx
+++ frontend/components/NoteIndexItem.jsx
@@ -1,13 +1,20 @@
 
 import React from 'react';
 import NoteLabelsIndexContainer from './NoteLabelsIndexContainer';
+import LabelEditFormContainer from './LabelEditForm';
 
 class NoteIndexItem extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = { showTray: "" };
+    this.state = { labelFormShow: false };
     this.handleClick = this.handleClick.bind(this);
+    this.toggleLabelForm = this.toggleLabelForm.bind(this);
+  }
+
+  toggleLabelForm(e) {
+    e.stopPropagation();
+    this.setState({ labelFormShow: !this.state.labelFormShow });
   }
 
   handleClick(e) {
@@ -19,6 +26,10 @@ class NoteIndexItem extends React.Component {
   
     const { note } = this.props;
 
+    const labelForm = this.state.labelFormShow ? (  
+      <LabelEditFormContainer />
+    ) : ( null );
+
     return (
     
       <li key={note.id} onClick={this.handleClick} className="note-item">
@@ -29,14 +40,17 @@ class NoteIndexItem extends React.Component {
         </div>
 
         <div className="note-item-label-tray">
-          <NoteLabelsIndexContainer labelIds={note.label_ids}/>
-        </div>  
+          <NoteLabelsIndexContainer labelIds={note.label_ids} />
+        </div>
 
         <div className={`note-item-tray`}>
-          <i className="fas fa-tag tray-item"></i>
+          <i className="fas fa-tag tray-item" onClick={this.toggleLabelForm}></i>
           <i className="far fa-trash-alt tray-item"></i>
         </div>
 
+        <div className="note-item-label-edit-form-container">
+          { labelForm }
+        </div>
       </li>
     )
   }
