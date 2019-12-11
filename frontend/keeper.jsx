84e5b6b3511f938994992f7866474f4cf2bdeeb6
thejenkdog <thejenkdog@gmail.com>
2019-12-11 15:07:22 -0800

    Refactor sidebar show, show hide logic, now can toggle a className, show and hide

--- frontend/keeper.jsx
+++ frontend/keeper.jsx
@@ -3,38 +3,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 import Root from './components/Root';
-import autosize from 'autosize';
-import { fetchNotes } from './actions/note_actions';
-import { updateFilter, deleteFilter } from './actions/filter_actions';
 
 
 document.addEventListener("DOMContentLoaded", () => {
   
   let store;
 
-  // bootstrap the current_user; see also root.html.erb
   if (window.currentUser) {
+    
     const preloadedState = {
       entities: {
         users: { [window.currentUser.id]: window.currentUser }
-      },
+          },
       session: { id: window.currentUser.id }
     };
+
     store = configureStore(preloadedState);
     delete window.currentUser;
+  
   } else {
+  
     store = configureStore();
+  
   }
-
-  // Testing begins
-  // window.getState = store.getState;
-  // window.dispatch = store.dispatch;
-  // Testing ends
 
   const root = document.getElementById("root");
   ReactDOM.render(<Root store={store}/>, root);
 
-  // using autosize library to make text area in note forms auto-resizing
-  // const textArea = document.querySelector('.note-create-body');
-  // autosize(textArea);
 });
